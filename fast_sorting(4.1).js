31326eef988b5bba289834b9746b7907f5f39da0
sham  pun <begasos1997@gmail.com>
2022-10-15 14:58:17 +0400

    Рекурсия

--- fast_sorting(4.1).js
+++ fast_sorting(4.1).js
@@ -1,5 +1,5 @@
 //Сумма элементов массива
-let arr = [1, 2, 3, 4, 5, 6] 
+let arr = [1,2,3,4,5,6] 
 
 function add(arr) {
     if (arr.length == 1){ // base case
@@ -7,7 +7,7 @@ function add(arr) {
     } 
     return arr[0] + add(arr.slice(1))  // recurse
 }
-console.log(add(arr))
+//console.log(add(arr))
 
 
 // Подсчёт колличества элеметнов в массиве
@@ -17,9 +17,19 @@ function count(arr) {
     }
     return 1 + count(arr.slice(1));
 }
+//console.log(count(arr))
 
-let x = [1,1]
-
-console.log(count(x))
 
 //найти наибольшее число в списке
+function max(arr) {
+    if (arr.length == 1) { 
+        return arr[0]; 
+    }else if (arr[0] < arr[1]) { 
+        arr.splice(0,1); 
+    }else { 
+        arr.splice(1,1); 
+    }
+    return max(arr);
+}
+
+//console.log(max(arr))
