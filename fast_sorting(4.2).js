f9b8fb6f5e706c6c551efd3fdd26c507c7518616
sham  pun <begasos1997@gmail.com>
2022-12-02 17:16:51 +0400

    продолжаем

--- fast_sorting(4.2).js
+++ fast_sorting(4.2).js
@@ -3,4 +3,20 @@ var array = [3,7,2,8,2,782,7,29,1,3,0,34];
     {
         return a-b;
     });
-    console.log(array); 
+    //console.log(array); 
+
+//Быстрая сортировка с разделением
+
+let arr = [10,33,15]
+
+
+function quicksort(arr){
+    if (arr.length < 2){ // Массивы с 1 или 2 элементами нет смысла сортировать.
+        return arr
+    };
+    let mid = arr[0]; // Опорный элемент, может быть любым.
+    
+}
+
+quicksort(arr)
+
