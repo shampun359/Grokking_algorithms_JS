72aa66f3ff8fa4059af6734451a26bd20d8ef933
sham  pun <begasos1997@gmail.com>
2023-01-12 23:23:20 +0400

    JS
    switch

--- trash.js
+++ trash.js
@@ -1,5 +1,28 @@
-let user ;
+/*const number = +prompt('Введите число между 0 и 3', '');
 
-alert(user ?? "Аноним"); 
+if (number === 0) {
+  alert('Вы ввели число 0');
+}
 
+if (number === 1) {
+  alert('Вы ввели число 1');
+}
 
+if (number === 2 || number === 3) {
+  alert('Вы ввели число 2, а может и 3');
+}*/
+
+let number = prompt('Введите число между 0 и 3', '');
+
+switch (number){
+  case '0':
+    console.log('Вы ввели число 0');
+    break
+  case '1':
+    console.log('Вы ввели число 1');
+    break
+  case '2':
+  case '3':
+    console.log('Вы ввели число 2, а может и 3');
+    break
+}
