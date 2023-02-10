d5474e63493ce6e915278b87ddebe12b6799bed1
sham  pun <begasos1997@gmail.com>
2023-02-10 14:38:02 +0400

    Практикую

--- trash.js
+++ trash.js
@@ -1,27 +1,11 @@
-console.time('test-Рекурсия');
 
-function fact(x){
-  if (x == 1){
-      return 1
-  }else{
-      return x + fact(x-1)
-  }
-  
+function checkAge(age) {
+    return age > 18 ? true : false
 }
-console.log(fact(5000))
 
-console.timeEnd('test-Рекурсия');
-
-console.time('test');
-
-var summation = function (num) {
-  let result = 0;
-   for (var i = 1; i <= num; i++) {
-     result += i;
-   }
-  return result;
+function checkAgeTwo(age) {
+    return age > 18 || false
 }
- 
-console.log(summation(5000))
 
-console.timeEnd('test');
+console.log(checkAge(15))
+console.log(checkAgeTwo(15))
