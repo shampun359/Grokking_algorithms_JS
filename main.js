11b7517a9faab09ad58048dbe19bb2e7cc933a86
sham  pun <begasos1997@gmail.com>
2023-03-18 17:53:22 +0400

    Obj
    функции-  конструкторы

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-let user = { 
-  name: "John" 
-};
+function Calculator() {
 
-user.f = sayHi;
-console.log(user)
+  this.read = function() {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
+  };
+
+  this.sum = function() {
+    return this.a + this.b;
+  };
+
+  this.mul = function() {
+    return this.a * this.b;
+  };
+}
+
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Sum=" + calculator.sum() );
+alert( "Mul=" + calculator.mul() );
